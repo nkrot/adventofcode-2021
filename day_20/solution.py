726a8f42f3bca22dafecb8e67b091175626dd0f8
Nikolai Krot <talpus@gmail.com>
2021-12-22 09:16:39 +0100

    solve day 20 p1 and p2

--- day_20/solution.py
+++ day_20/solution.py
@@ -4,34 +4,117 @@
 #
 #
 
-import re
 import os
 import sys
-from typing import List
-from collections import defaultdict
+from typing import List, Union
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from aoc import utils
+
 
 DAY = '20'
 DEBUG = int(os.environ.get('DEBUG', 0))
 
 
+class ImageEnhancementAlgorithm(object):
+
+    def __init__(self, data: str):
+        self.data = list(data)
+
+    def show_table(self):
+        """conversion table"""
+        for idx, ch in enumerate(self.data):
+            bn = f"{idx:0>9b}"
+            s = "".join(["#" if int(n) % 2 else "." for n in list(bn)])
+            print("{}: {} {} --> {}".format(idx, bn, s, ch))
+
+    # TODO: this method is called crazy amount of times. optimize it?
+    def recode(self, seq: Union[str, List[Union['Pixel', int]]]) -> str:
+        """Given a stride of a pixel, return new pixel value that results
+        from the stride according to the algorithm.
+        """
+        if isinstance(seq, str):
+            ints = [Pixel.VALUES[ch] for ch in list(seq)]
+        else:
+            ints = [int(p) for p in seq]
+        binary_string = "".join(str(i) for i in ints)
+        dec = int(binary_string, 2)
+        return self.data[dec]
+
+    def enhance(self, image: 'Image'):
+        """Enhance given image once, modifying it in place"""
+
+        if DEBUG > 1:
+            print("--- enhancing ---")
+
+        # After one round of image enhancement, the image will grow in size
+        # one pixel on each side. Here we prepare the image for that by adding
+        # a padding of size 1.
+        image.pad(image.outside_pixel)
+
+        # compute new pixel values but do not update the pixels themselves
+        new_pixel_values = []
+        for pos, pixel in image:
+            stride = image.get_stride(pos)
+            new_value = self.recode(stride)
+            if pixel.value != new_value:
+                new_pixel_values.append((pixel, new_value))
+
+        # update pixel values
+        for pixel, new_value in new_pixel_values:
+            pixel.value = new_value
+
+        # update outside pixel (InfinitePixel)
+        image.outside_pixel = next(image.outside_pixel)
+
+        if DEBUG > 1:
+            print("Number of updated pixels", len(new_pixel_values))
+            image.inspect("Enhanced image")
+
+
 class Pixel(object):
 
-    VALUES = {".": 0, "#": 1}
+    VALUES = {'.': 0, '#': 1}
 
-    def __init__(self, value: str = "."):
+    def __init__(self, value: Union[str, 'Pixel'] = '.'):
         self.value = str(value)
 
     def __int__(self):
         return self.VALUES[self.value]
+
+    # removed to improve runtime speed
+    # def __eq__(self, other: Union[int, str, 'Pixel']):
+    #     if isinstance(other, (str, int)):
+    #         return type(other)(self) == other
+    #     elif isinstance(other, type(self)):
+    #         return str(self) == str(other)
+    #     raise Exception("Non-comparable type: {}".format(type(other)))
+
+    # removed to improve runtime speed
+    # def __ne__(self, other):
+    #     return not(self.__eq__(other))
 
     def __str__(self):
         return str(self.value)
 
     def __repr__(self):
         return "<{}: value={}>".format(self.__class__.__name__, self.value)
+
+
+class InfinityPixel(Pixel):
+    """A special kind of pixel that knows how it should change in the next step"""
+
+    def __init__(self, initial_value, changer):
+        super().__init__(initial_value)
+        self.changer = changer
+
+    def __next__(self):
+        """Return next state of the pixel that is surrounded by pixels of same
+        kind, like a dark pixel surrounded by dark pixels or a light pixel
+        surrounded by light pixels."""
+        stride = [int(self)] * 9
+        new_value = self.changer.recode(stride)
+        return self.__class__(new_value, self.changer)
 
 
 class Image(object):
@@ -50,160 +133,183 @@ class Image(object):
 
     def __init__(self, pixels):
         self.pixels = pixels
-        self.padding = {}
+        self.outside_pixel = None
+        self._set_spans()
+        # self._strides = {}
 
     def __iter__(self):
         pixels = list(self.pixels.items())
-        pixels.extend(list(self.padding.items()))
         return iter(pixels)
 
     def __len__(self):
         return len(self.pixels)
 
+    def _set_spans(self):
+        """Sets self.xspan and self.yspan variables"""
+        xs, ys = set(), set()
+        for (x, y) in self.pixels:
+            xs.add(x)
+            ys.add(y)
+        xs = sorted(xs)
+        ys = sorted(ys)
+        self.xspan = [xs[0], xs[-1]]
+        self.yspan = [ys[0], ys[-1]]
+
+    # def get_stride2(self, xy) -> List[Pixel]:
+    #     """Return pixels in the 3x3 window around the position <xy>.
+    #     This method maintains a cache of answers of the form
+    #       strides[xy] = [(x1, y1), (x2,y2), ..., (x9, y9)]
+    #     It turns out that this approach makes performance worse.
+    #     """
+    #     if xy not in self._strides:
+    #         x, y = xy
+    #         self._strides[xy] = [(x+dx, y+dy) for dx, dy in self.STRIDE]
+    #     stride = [self.pixels.get(_xy, self.outside_pixel)
+    #                 for _xy in self._strides[xy]]
+    #     return stride
+
     def get_stride(self, xy) -> List[Pixel]:
-        """create if necessary"""
+        """Return pixels in the 3x3 window around the position <xy>"""
         x, y = xy
-        stride = []
-        for dx, dy in self.STRIDE:
+        stride = [0] * 9
+        for idx, (dx, dy) in enumerate(self.STRIDE):
             xy = x+dx, y+dy
-            pixel = self.pixels.get(xy, self.padding.get(xy, Pixel()))
-            stride.append(pixel)
+            stride[idx] = self.pixels.get(xy, self.outside_pixel)
         return stride
 
-    def pad(self, value='.'):
-        """Add temporary padding (should be removed with .unpad())"""
-        self.padding = {}
-        xs, ys = self.xs(), self.ys()
-        minx, maxx = xs[0]-1, xs[-1]+1
-        miny, maxy = ys[0]-1, ys[-1]+1
-        for x in range(minx, maxx+1):
+    def pad(self, value: Union[str, 'Pixel'] = '.'):
+        """Add padding along the borders (outside) of the current image"""
+        self.xspan = [self.xspan[0]-1, self.xspan[1]+1]
+        self.yspan = [self.yspan[0]-1, self.yspan[1]+1]
+        minx, maxx = self.xspan
+        miny, maxy = self.yspan
+        for x in (minx, maxx):
             for y in range(miny, maxy+1):
-                xy = (x,y)
-                if xy not in self.pixels:
-                    pix = Pixel(value)
-                    # self.pixels[xy] = pix # did not work
-                    self.padding[xy] = pix
+                self.pixels[(x,y)] = Pixel(value)
+        for y in (miny, maxy):
+            for x in range(minx, maxx+1):
+                self.pixels[(x,y)] = Pixel(value)
 
-    def extend(self, value='.'):
-        """Add padding immediately"""
-        self.pad(value)
-        for xy, pixel in self.padding.items():
-            self.pixels[xy] = pixel
-        self.padding = {}
-
-    def unpad(self):
-        """Keep padding that has light pixels and remove padding consisting of
-        all dark pixels only"""
-        for xy, pixel in self.padding.items():
-            if int(pixel) == 1:
-                assert xy not in self.pixels, "Oh shit"
-                self.pixels[xy] = pixel
-        self.padding = {}
-        # adjust the shape of the image to rectangle
-        for x in self.xs():
-            for y in self.ys():
-                xy = (x, y)
-                if xy not in self.pixels:
-                    self.pixels[xy] = Pixel()
-
-    def num_lit_pixels(self):
+    def count_lit_pixels(self):
         return sum(int(pix) for pix in self.pixels.values())
 
     def shape(self):
-        return (len(self.xs()), len(self.ys()))
-
-    def xs(self):
-        xs = set()
-        xys = list(self.pixels.keys()) + list(self.padding.keys())
-        for xy in xys:
-            xs.add(xy[0])
-        return sorted(xs)
-
-    def ys(self):
-        ys = set()
-        xys = list(self.pixels.keys()) + list(self.padding.keys())
-        for xy in xys:
-            ys.add(xy[1])
-        return sorted(ys)
+        xdim = 1 + self.xspan[1] - self.xspan[0]
+        ydim = 1 + self.yspan[1] - self.yspan[0]
+        return (xdim, ydim)
 
     def __str__(self):
-        xs, ys = self.xs(), self.ys()
+        minx, maxx = self.xspan
+        miny, maxy = self.xspan
         out = ''
-        for x in self.xs():
-            for y in self.ys():
+        for x in range(minx, maxx+1):
+            for y in range(miny, maxy+1):
                 xy = (x, y)
-                pixel = self.pixels.get(xy, self.padding.get(xy))
+                pixel = self.pixels.get(xy, xy)
                 out += str(pixel)
             out += '\n'
         return out
 
+    def inspect(self, title='Image'):
+        print(f"--- {title} ---")
+        print("Image shape: {}, Number of lit pixels: {}".format(
+            self.shape(), self.count_lit_pixels()))
+        print("Outside pixel:", repr(self.outside_pixel))
+        print(str(self))
+
+
+def test_iea_1():
+    data = utils.load_input("test.1.txt")[0]
+    # data = utils.load_input("input.txt")[0]
+    algo = ImageEnhancementAlgorithm(data)
+    algo.show_table()
+
+
+def test_iea_2():
+    data = utils.load_input("test.1.txt")[0]
+    algo = ImageEnhancementAlgorithm(data)
+    tests = [(".........", ".")]
+    for input, exp in tests:
+        res = algo.recode(input)
+        print(exp == res, exp, res)
+        pixels = [Pixel(ch) for ch in input]
+        res = algo.recode(pixels)
+        print(exp == res, exp, res)
+
+
+def test_pixel():
+    tests = [
+            (Pixel('.'), 0, "."),
+            (Pixel('#'), 1, "#")
+            ]
+    for pix, num, string in tests:
+        print("exists?", bool(pix))
+        print("numeric value?", num == int(pix), num, int(pix))
+        print("string value?", string == str(pix), string, str(pix))
+        # corresponding methods __eq__ and __ne__ were removed to improve runtime
+        # print("Equal to number?", pix == num)
+        # print("Equal to number?", num == pix)
+        # print("Non-Equal to number?", pix != 2)
+        # print("Non-Equal to number?", 2 != pix)
+        # print("Equal to string?", pix == string)
+        # print("Equal to string?", string == pix)
+        # print("Non-Equal to string?", pix != 'x')
+        # print("Non-Equal to string?", 'x' != pix)
+    pix1, pix2 = tests[0][0], tests[1][0]
+    print(pix1 == pix1)
+    print(pix1 != pix2)
+
+
+def test_infinity_pixel():
+    data = utils.load_input("input.txt")[0]
+    algo = ImageEnhancementAlgorithm(data)
+    pixel = InfinityPixel(".", algo)
+    exp = [".", "#", ".", "#", ".", "#"]
+    pixels = [pixel]
+    for _ in range(5):
+        pixel = next(pixel)
+        pixels.append(pixel)
+    res = list(map(str, pixels))
+    print(exp == res, exp, res)
+
+
+# test_iea_1()
+# test_iea_2()
+# test_pixel()
+# test_infinity_pixel()
+# exit(100)
+
 
 def parse_input(lines: List[str]):
-    algo = lines.pop(0)
-    return algo, Image.from_lines(lines[1:])
-
-
-# What happens to pixels in the infinity
-# 1) a . that is completely surrounded by . turn to # (ON)
-# 2) a # that is completely surrounded by # turns to . (OFF)
-# With number of iterations being even, inifinity is all OFF (.)
-# but what happens in the proximity of the image?
-
-
-def enhance(image, algo, step):
-    print("--- enhancing ---")
-    char = '.'
-    #char = '#' if step % 2 else '.'
-    #print("Padding with ", char)
-    image.pad(char)
-    print("Initial image {}\n{}".format(image.shape(), image))
-    new_pixel_values = []
-    # compute new pixel values
-    for pos, pixel in image:
-        # print(pos, pixel)
-        stride = image.get_stride(pos)
-        # print("", "".join([str(p) for p in stride]))
-        binseq = "".join([str(int(pix)) for pix in stride])
-        dec = int(binseq, 2)
-        new_value = algo[dec]
-        # print(binseq, dec, new_value)
-        if pixel.value != new_value:
-            new_pixel_values.append((pixel, new_value))
-    # update pixel values
-    print("Number of pixels to update", len(new_pixel_values))
-    for pixel, value in new_pixel_values:
-        pixel.value = value
-    image.unpad()
-    print("Resulting image {}\n{}".format(image.shape(), image))
+    algo = ImageEnhancementAlgorithm(lines[0])
+    image = Image.from_lines(lines[2:])
+    return algo, image
 
 
 def solve_p1(lines: List[str], times=2) -> int:
     """Solution to the 1st part of the challenge"""
     algo, image = parse_input(lines)
-    print(image.shape(), image.num_lit_pixels())
+    image.outside_pixel = InfinityPixel(".", algo)
+
+    if DEBUG:
+        image.inspect("Initial image")
+
     for t in range(times):
-        enhance(image, algo, t)
-        print("Number of lit pixels", image.num_lit_pixels())
-    return image.num_lit_pixels()
+        algo.enhance(image)
+
+    if DEBUG:
+        image.inspect(f"Final image, enhanced {times} times")
+
+    return image.count_lit_pixels()
 
 
 def solve_p2(lines: List[str]) -> int:
     """Solution to the 2nd part of the challenge"""
-    # TODO
-    return 0
+    return solve_p1(lines, 50)
 
 
 tests = [
-   (utils.load_input('test.1.txt'), 35, None),
-    # (utils.load_input('test.2.txt'), -43, None),
-
-    # After one iteration:
-    # test.3.txt shows that a . surrounded all by . turns to #
-    # (utils.load_input('test.3.txt'), -43, None),
-
-    # test.4.txt shows that a # surrounded all by # turn to .
-    # check the central 3x3 area!
-    #(utils.load_input('test.4.txt'), -43, None),
+   (utils.load_input('test.1.txt'), 35, 3351),
 ]
 
 
@@ -224,18 +330,20 @@ def run_real():
     lines = utils.load_input()
 
     print(f"--- Day {DAY} p.1 ---")
-    exp1 = -4525  # too low
-    exp1 = -4846  # too low
-    exp1 = -4942  # too high
+    exp1 = 4873
     res1 = solve_p1(lines)
     print(exp1 == res1, exp1, res1)
 
     print(f"--- Day {DAY} p.2 ---")
-    exp2 = -1
+    exp2 = 16394
     res2 = solve_p2(lines)
     print(exp2 == res2, exp2, res2)
 
 
-if __name__ == '__main__':
+def main():
     run_tests()
-#    run_real()
+    run_real()
+
+
+if __name__ == '__main__':
+    main()
